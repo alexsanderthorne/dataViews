ee4e749211f31c7e6f52b1686c27f0077690b392
alexsanderthorne <alexsanderthorne@gmail.com>
2021-10-04 20:50:04 -0300

    titanic . . .

--- newPlots.py
+++ newPlots.py
@@ -5,3 +5,9 @@ import matplotlib.pyplot as plt
 fig, axes = plt.subplots(ncols=2,nrows=2, figsize=(15,10))
 
 print(fig,axes)
+
+dataset_2 = anscombe[anscombe['dataset']=='II']
+dataset_3 = anscombe[anscombe['dataset']=='III']
+dataset_4 = anscombe[anscombe['dataset']=='IV']
+
+axes[0,0].plot(data_se)
